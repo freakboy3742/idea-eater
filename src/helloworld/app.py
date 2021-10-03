c9280915b5eadc206c9efd7c4e88f2fea4e6bd72
Lilly Ryan <lilly@zagreus.local>
2021-10-03 15:45:11 +1100

    changing labels to make the app more idea-eater-y

--- src/helloworld/app.py
+++ src/helloworld/app.py
@@ -6,39 +6,40 @@ from toga.style import Pack
 from toga.style.pack import COLUMN, ROW
 
 
-class HelloWorld(toga.App):
+class IdeaEater(toga.App):
 
     def startup(self):
         main_box = toga.Box(style=Pack(direction=COLUMN))
 
-        name_label = toga.Label(
-            'Your name: ',
+        idea_label = toga.Label(
+            'Thought goes here: ',
             style=Pack(padding=(0, 5))
         )
-        self.name_input = toga.TextInput(style=Pack(flex=1))
+        self.idea_input = toga.TextInput(style=Pack(flex=1))
 
-        name_box = toga.Box(style=Pack(direction=ROW, padding=5))
-        name_box.add(name_label)
-        name_box.add(self.name_input)
+        idea_box = toga.Box(style=Pack(direction=ROW, padding=5))
+        idea_box.add(idea_label)
+        idea_box.add(self.idea_input)
 
         button = toga.Button(
-            'Say Hello!',
-            on_press=self.say_hello,
+            'Nom',
+            on_press=self.eat_idea,
             style=Pack(padding=5)
         )
 
-        main_box.add(name_box)
+        main_box.add(idea_box)
         main_box.add(button)
 
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
         self.main_window.show()
 
-    def say_hello(self, widget):
+    def eat_idea(self, widget):
         self.main_window.info_dialog(
             'Hi there!',
-            "Hello, {}".format(self.name_input.value)
+            "Hello, {}".format(self.idea_input.value)
         )
 
+
 def main():
-    return HelloWorld()
+    return IdeaEater()
